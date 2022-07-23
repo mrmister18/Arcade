430e7f86acae7264b86a6fdb09eb03ede810079b
Isaac <isaachernandez8@gmail.com>
2022-07-23 11:24:09 -0700

    made starting player random and added switchPlayer function

--- app.js
+++ app.js
@@ -6,5 +6,9 @@ let gameState = {
       [null, null, null]
     ]
   }
+  let currentPlayer = gameState.players[Math.round(Math.random())]
 
-  
+function switchPlayer() {
+    if (currentPlayer === "x") {currentPlayer = "o"}
+    else if (currentPlayer === "o") {currentPlayer = "x"}
+}
