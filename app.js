79c1457a757be6b4558b7ed895a848573698c60e
Isaac <isaachernandez8@gmail.com>
2022-07-28 21:28:30 -0700

    created checkWin function, scrapped initial html and css to replace with retro looking style, start DOM manipulation for number of players

--- app.js
+++ app.js
@@ -18,3 +18,43 @@ function inputTurn(x, y) {
     gameState.board[y][x] = currentPlayer
     switchPlayer()}
 }
+
+function checkAcross() {
+  for (let i = 0; i < gameState.board.length; i++) {
+    let boardRow = gameState.board[i]
+    if (boardRow[0] === boardRow[1] && boardRow[1] === boardRow[2] && boardRow[0]) {
+      return "You Win!"
+    }
+  }
+}
+
+function checkVertical() {
+  for (let i = 0; i < gameState.board.length; i++) {
+    if (gameState.board[0][i] === gameState.board[1][i] && gameState.board[1][i] === gameState.board[2][i] && gameState.board[0][i]) {
+      return "You Win!"
+    }
+  }
+}
+
+function checkDiagonal() {
+  if (gameState.board[0][0] === gameState.board[1][1] && gameState.board[1][1] === gameState.board[2][2] && gameState.board[0][0]) {
+    console.log("You Win!")
+  } else if (gameState.board[0][2] === gameState.board[1][1] && gameState.board[1][1] === gameState.board[2][0] && gameState.board[1][1]) {
+    console.log("You Win!")
+  }
+}
+
+function checkWin() {
+  checkAcross()
+  checkVertical()
+  checkDiagonal()
+}
+
+let onePlayer = document.getElementById("1Player")
+let twoPlayer = document.getElementById("2Player")
+let playerName = document.createElement("input")
+
+onePlayer.addEventListener("click", function() {
+  onePlayer.replaceWith(playerName)
+  twoPlayer.innerText = "Computer"
+})
